abf42dfaf98d1797c3ccc87e9728a3054be1c5cd
Mylie Walker <myliewalker@Mylies-MacBook-Pro.local>
2019-01-24 15:07:06 -0500

    initial version

---
+++ divby7or11.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int num, char* args[]) {
+    int total;
+    sscanf(args[num-1], "%d", &total);
+    int count = 0;
+    int current = 7;
+    int numbers[total];
+
+    while (count < total) {
+        if (current%7 == 0 || current%11 == 0) {
+            numbers[count] = current;
+            count++;
+        }
+        current++;
+    }
+
+    for (int i = 0; i < total; i++) {
+        printf("%d\n", numbers[i]);
+    }
+    //what if num_values = 0
+
+    return 0;
+}
---
+++ recurse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int f(int n);
+
+int main(int num, char* args[]) {
+    int n;
+    sscanf(args[num-1], "%d", &n);
+    int fib = f(n);
+    printf("%d", fib);
+    return 0;
+}
+
+int f(int n) {
+    if (n == 0) return 5;
+    return 4*n + 2*f(n-1) + 3;
+}
