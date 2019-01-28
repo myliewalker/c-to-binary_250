2acba7ab40a2c93353fe451daa2f3f18e70e8312
Mylie Walker <myliewalker@Mylies-MacBook-Pro.local>
2019-01-28 16:38:20 -0500

    correct unsorted output

--- HoopsStats.c
+++ HoopsStats.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
 typedef struct player {
@@ -12,14 +13,13 @@ typedef struct player {
 
 FILE *fr;
 
-int sort(bb_player* players, bb_player low, bb_player high, int start, int remaining);
+int sort(bb_player *players, int count);
 int swap(bb_player* players, bb_player a, bb_player b, int loc);
 
 int main(int num, char* args[]) {
     char* file_name = args[num-1];
-    bb_player* players = NULL;
-    bb_player* start = players;
-    bb_player* tmp = NULL;
+    bb_player *players = NULL;
+    bb_player *tmp = NULL;
     int count = 0;
     char line[80];
 
@@ -31,64 +31,57 @@ int main(int num, char* args[]) {
 
     while(fgets(line, 80, fr) != NULL) {
         if (strncmp(line, "DONE\n", 80) == 0) break;
-        //can we assume done will end with a new line??
 
-        char temp_name[80];
-        int temp_num;
-        float temp_points;
-        int temp_year;
-        bb_player* p = (bb_player*)malloc(sizeof(bb_player));
+        char player_name[80];
+        int player_number;
+        float player_points;
+        int player_year;
         
-        sscanf(line, "%s", p[0].name);
+        sscanf(line, "%s", player_name);
         fgets(line, 80, fr);
-        sscanf(line, "%d", &temp_num);
+        sscanf(line, "%d", &player_number);
         fgets(line, 80, fr);
-        sscanf(line, "%f", &temp_points);
+        sscanf(line, "%f", &player_points);
         fgets(line, 80, fr);
-        sscanf(line, "%d", &temp_year);
+        sscanf(line, "%d", &player_year);
        
-        p[0].number = temp_num;
-        p[0].avg_points = temp_points;
-        p[0].year = temp_year;
-
-        // printf("%s %d %f %d \n", p[0].name, temp_num, temp_points, temp_year);
-        // players = p;
-        // count++;
-        // break;
+        bb_player *player = (bb_player*) malloc(sizeof(bb_player));
+        strcpy(player->name, player_name);
+        player->number = player_number;
+        player->avg_points = player_points;
+        player->year = player_year;
+        player->next = NULL;
 
         if(players == NULL) {
-            players = p;
-            tmp = p;
+            players = player;
+            tmp = player;
         }
         else {
-            tmp->next = p;
+            tmp->next = player;
             tmp = tmp->next;
         }
         count++;
     }
 
-    // sort(players, players[0], players[1], 0, count-1);
+    sort(players, count);
 
-    for (int i = 0; i < count; i++) {
-        printf("%s %d %d\n", players[i].name, players[i].number, players[i].year);
+    tmp = players;
+    while (tmp != NULL) {
+        printf("%s %d %d\n", tmp->name, tmp->number, tmp->year);
+        tmp = tmp->next;
     }
-    // while (players != NULL) {
-    //     printf(players.name, players.number, players.year);
-    // }
 
-    players = start;
-
-    // free(players);
     while (players != NULL) {
         bb_player *tmp = players;
         players = players->next;
         free(tmp);
     }
+
     fclose(fr);
     return 0;
 }
 
-int sort(bb_player* players, bb_player low, bb_player high, int start, int remaining) {
+int sort(bb_player *players, int count) {
     if (remaining == 0) return 0;
 
     if (low.avg_points == high.avg_points) {
